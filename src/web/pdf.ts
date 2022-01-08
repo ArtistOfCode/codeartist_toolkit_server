f3c2730395e460451114af80ddfcbbd8f0f719b5
AiJiangnan <904629998@qq.com>
2022-01-08 21:02:15 +0800

    完成PDF合并

--- src/web/pdf.ts
+++ src/web/pdf.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 import * as multer from 'multer';
 import * as path from 'path';
-import ErrorRsp, { HttpStatus } from './error_resp';
-import Parser from '../util/Parser';
 import EXE from '../core/exe_driver';
+import Parser from '../util/Parser';
+import ErrorRsp, { HttpStatus } from './error_resp';
 
 const dest = path.join(process.cwd(), '/tmp');
 const router = Router();
@@ -27,22 +27,22 @@ router.post('/', upload.single('file'), (req, res) => {
 
 router.post('/merge', (req, res) => {
     const { filename, pdfs } = req.body;
-    if (!pdfs || pdfs.length < 2) {
+    if (!pdfs || pdfs.length < 1) {
         res.status(HttpStatus.BAD_REQUEST).json(ErrorRsp.of('文件数量异常'));
     }
 
-    const args = pdfs.map((pdf: string) => path.join(dest, pdf)).join(' ')
+    let file = '', pageArgs = '';
+    (pdfs as { page: string, name: string }[])
+        .forEach((pdf, i) => {
+            const label = String.fromCharCode(65 + i);
+            file += `${label}=${path.join(dest, pdf.name)} `;
+            pageArgs += `${label}${pdf.page} `;
+        });
+
     const mergeFilename = path.join(dest, (filename || Date.now()) + '.pdf')
 
-    EXE.pdf(`${args} output ${mergeFilename}`)
-        .then(data => {
-            res.download(mergeFilename);
-            // res.end(() => {
-            //     // if (fs.existsSync(dest)) {
-            //     //     delDir(dest)
-            //     // }
-            // })
-        })
+    EXE.pdf(`${file}cat ${pageArgs}output ${mergeFilename}`)
+        .then(data => { res.download(mergeFilename); })
         .catch(err => res.status(HttpStatus.INTERNAL_SERVER_ERROR)
             .json(ErrorRsp.of('PDF合并异常', err)))
 })
